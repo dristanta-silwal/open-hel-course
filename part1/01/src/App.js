757942c342c1aa69fd01208e52050937ed59cb94
Dristanta Silwal <you@example.com>
2022-07-07 09:46:36 +0545

    part 02 completed

--- part1/01/src/App.js
+++ part1/01/src/App.js
@@ -1,13 +1,41 @@
 const App = () => {
+  // Header
   const Header = (props) => {
     return (
       <h1>{props.course}</h1>
     )
   }
 
+  // Part
+  const Part = (props) => {
+    return (
+      <p>{props.name} {props.ex}</p>
+    )
+  }
+
+  // Content
+  const Content = () => {
+    return (
+      <div>
+        <Part name="Fundamentals of React" ex="10" />
+        <Part name="Using props to pass data'" ex="7" />
+        <Part name="State of a component" ex="14" />
+      </div>
+    )
+  }
+
+  // Total
+  const Total = () => {
+    return (
+      <p>Number of exercises {10 + 7 + 14}</p>
+    )
+  }
+
   return (
     <div>
       <Header course="Half Stack application development" />
+      <Content />
+      <Total />
     </div>
   )
 }
