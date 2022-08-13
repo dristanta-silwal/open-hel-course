2288f830aac38970f9d893474c3dd1f57b62fa68
Dristanta Silwal <you@example.com>
2022-08-13 08:57:25 +0545

    2.15 to 2.18 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,58 +1,76 @@
-import { useState } from "react";
-import Filter from "./filter";
-import PersonForm from "./form";
-import Persons from "./Persons";
+import React, { useState, useRef, useEffect } from 'react';
+import personService from './pers';
+import Filter from './filter';
+import PersonForm from './form';
+import Persons from './Persons';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456", id: 1 },
-        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    ]);
-    const [newPerson, setNewPerson] = useState({ name: "", number: "" });
-    const [filter, setFilter] = useState("");
-    const [personsToShow, setPersonsToShow] = useState(persons);
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [searchName, setSearchName] = useState('');
+    const focusName = useRef();
 
-    const addPerson = (event) => {
+    // Fetch persons array from json-server once the component is rendered for the first time
+    useEffect(() => {
+        personService
+            .getAll()
+            .then(setPersons);
+    }, []);
+
+    // Function to add a person to the phonebook
+    const addPerson = event => {
         event.preventDefault();
-        const currentName = persons.filter(
-            (person) => person.name === newPerson.name
-        );
 
-        if (currentName.length === 0) {
-            setPersons(persons.concat(newPerson));
-            setPersonsToShow(personsToShow.concat(newPerson));
+        const existingPerson = persons.find(person => person.name === newName);
+
+        // Update person if newName already exists, otherwise add new person
+        if (existingPerson) {
+            if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+                personService
+                    .update(existingPerson.id, { ...existingPerson, number: newNumber })
+                    .then(updatedPerson => setPersons(persons.map(person => {
+                        return person.id === updatedPerson.id ? updatedPerson : person;
+                    })));
+
+                // Clear input data
+                setNewName('');
+                setNewNumber('');
+            }
         } else {
-            alert(`${newPerson.name} is already added to phonebook`);
+            personService
+                .create(newName, newNumber)
+                .then(newPerson => setPersons(persons.concat(newPerson)));
+
+            // Clear input data
+            setNewName('');
+            setNewNumber('');
         }
-        setNewPerson({ name: "", number: "" });
+
+        // Place focus back on name input
+        focusName.current.focus();
     };
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setNewPerson({ ...newPerson, [name]: value });
-    };
-
-    const filterByName = (event) => {
-        const search = event.target.value;
-        setFilter(search);
-        setPersonsToShow(
-            persons.filter((person) => person.name.toLowerCase().includes(search))
-        );
+    // Function to delete a person from the phonebook
+    const deletePerson = (id, name) => {
+        window.confirm(`Delete ${name}?`) &&
+            personService
+                .remove(id)
+                .then(response => response === 200 && setPersons(persons.filter(person => person.id !== id)));
     };
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <Filter filter={filter} filterByName={filterByName} />
-            <PersonForm
-                addPerson={addPerson}
-                newPerson={newPerson}
-                handleChange={handleChange}
+            <Filter searchName={searchName} setSearchName={setSearchName} />
+
+            <h3>Add a new</h3>
+            <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}
+                addPerson={addPerson} focusName={focusName}
             />
-            <h2>Numbers</h2>
-            <Persons personsToShow={personsToShow} />
+
+            <h3>Numbers</h3>
+            <Persons persons={persons} searchName={searchName} deletePerson={deletePerson} />
         </div>
     );
 };
--- part2/phonebook/src/Persons.js
+++ part2/phonebook/src/Persons.js
@@ -1,12 +1,23 @@
-import Person from "./Person";
+import React from 'react';
 
-const Persons = ({ personsToShow }) => {
+// Component for displaying all the persons in the phonebook
+
+const Persons = ({persons, searchName, deletePerson}) => {
     return (
-        <div>
-            {personsToShow.map((person) => (
-                <Person key={person.name} person={person} />
-            ))}
-        </div>
+        persons.length ?
+        <ul style={{listStyle: 'none', padding: 0}}>
+            {
+                persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+                .map(person => (
+                    <li key={person.name}>
+                        {person.name} {person.number} &nbsp;
+                        <button onClick={() => deletePerson(person.id, person.name)}>Delete</button>
+                    </li>
+                ))
+            }
+        </ul>
+        :
+        <p>Loading contacts...</p>
     );
 };
 
